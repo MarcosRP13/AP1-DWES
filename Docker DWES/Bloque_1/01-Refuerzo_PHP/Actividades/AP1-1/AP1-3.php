f33bdcaabf6b6740f29978ff33c779a781fc57e1
marope01 <marope01@alumnatflorida.es>
2025-09-11 12:08:02 +0200

    AP1-3 finalizado

--- Docker DWES/Bloque_1/01-Refuerzo_PHP/Actividades/AP1-1/AP1-3.php
+++ Docker DWES/Bloque_1/01-Refuerzo_PHP/Actividades/AP1-1/AP1-3.php
@@ -9,16 +9,31 @@ $pos = [
     11 => "sexto",
 ];
 
+$suma = 0;
+$contador = 1;
 foreach ($pos as $clave => $valor) {
-    if  ($valor == "primero" || $valor == "tercero" || $valor == "quinto"   ) {
+    if  ($contador == 1 || $contador == 3 || $contador == 5) {
         echo "Estas en una posicion impar". "<br>";
         $impar = true;
         $par = false;
+        $suma = $suma + $clave;
+        echo "$suma". "<br>";
     }
-    elseif ($valor == "segundo" || $valor == "cuarto" || $valor == "sexto") {
+    elseif ($contador == 2 || $contador == 4 || $contador == 6) {
         echo "Estas en una posicion par". "<br>";
         $par = true;
         $impar = false;
+        $suma = $suma + $clave;
+        echo "$suma". "<br>";
     }
-
+    if ($suma > 5) {
+        echo "El valor de la suma es mayor que 5" . "<br>";
+    }
+    elseif ($suma > 10) {
+        echo "El valor de la suma es mayor que 10" . "<br>";
+    }
+    elseif ($suma > 20) {
+        echo "El valor de la suma es mayor que 20" . "<br>";
+    }
+    $contador++;
 }
