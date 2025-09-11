fe109bcc402ee78df0c9f6cb0492e4995c81a6a5
marope01 <marope01@alumnatflorida.es>
2025-09-11 11:27:40 +0200

    AP1-1, 2 finalizado

--- Docker DWES/Bloque_1/01-Refuerzo_PHP/Actividades/AP1-1/AP1-1.php
+++ Docker DWES/Bloque_1/01-Refuerzo_PHP/Actividades/AP1-1/AP1-1.php
@@ -1,3 +1,10 @@
 <?php
-echo "hola";
-?>
+$info = [];
+
+foreach ($_GET as $clave => $valor) {
+$info[$clave] = $valor;
+};
+
+foreach ($info as $clave => $valor) {
+echo "Se ha recibido $valor para la clave $clave.<br>";
+};
