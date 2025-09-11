b29a2a0b1c55876781eaa3e9618b83db80e477ad
MARCOS RODRIGUEZ PEREZ <marope01@alumnatflorida.es>
2025-09-11 10:42:16 +0200

    Revert "AP1-1 finalizado"
    This reverts commit 92fdb75f16f18cb9561bb74031608093b4cd4b3a.

--- Docker DWES/Bloque_1/01-Refuerzo_PHP/Actividades/AP1-1/AP1-1.php
+++ Docker DWES/Bloque_1/01-Refuerzo_PHP/Actividades/AP1-1/AP1-1.php
@@ -1,13 +1,3 @@
 <?php
-
-$info = [];
-
-foreach ($_GET as $clave => $valor) {
-    $info[$clave] = $valor;
-};
-
-foreach ($info as $clave => $valor) {
-    echo "Se ha recibido $valor para la clave $clave.<br>";
-};
-
+echo "hola";
 ?>
