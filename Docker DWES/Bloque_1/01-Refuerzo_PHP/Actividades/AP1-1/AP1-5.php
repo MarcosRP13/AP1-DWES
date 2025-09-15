49ecc2e2737656406a11965ac2fa99b9f8cd63cd
Marcos <marcosrodriguez@gmail.com>
2025-09-15 20:13:39 +0200

    AP1-5 punto 5 finalizado

--- Docker DWES/Bloque_1/01-Refuerzo_PHP/Actividades/AP1-1/AP1-5.php
+++ Docker DWES/Bloque_1/01-Refuerzo_PHP/Actividades/AP1-1/AP1-5.php
@@ -1,23 +1,60 @@
-<?php
+    <?php
 
-$host = "mariadb-server";
-$user = "root";
-$pass = "root";
-$db = "AP1";
+    $host = "mariadb-server";
+    $user = "root";
+    $pass = "root";
+    $db = "AP1";
 
-$mysqli = new mysqli ($host,$user,$pass,$db);
+    $mysqli = new mysqli ($host,$user,$pass,$db);
 
-if ($mysqli->connect_errno) {
-    echo "Fallo al conectar a MySQL: " . $mysqli->connect_error;
-}
+    if ($mysqli->connect_error) {
+        echo "Fallo al conectar a MySQL: " . $mysqli->connect_error;
+    }
 
-// echo "Conectado a la base de datos";
+    // echo "Conectado a la base de datos";
 
-$sql = "SELECT * FROM usuarios";
-$resultado = $mysqli->query($sql);
-while ($fila = $resultado->fetch_assoc()) {
-    echo "ID: " . $fila["id"] . "<br>";
-    echo "Nombre: " . $fila["nombre"] . "<br>";
-    echo "Estado: " . $fila["estado"] . "<br>";
-}
+    $sql = "SELECT * FROM usuarios";
+    $resultado = $mysqli->query($sql);
+    while ($fila = $resultado->fetch_assoc()) {
+        echo "ID: " . $fila["id"] . "<br>";
+        echo "Nombre: " . $fila["nombre"] . "<br>";
+        echo "Estado: " . $fila["estado"] . "<br>";
+    }
 
+    $nombre = "Aladin";
+    $estado = "0    ";
+
+    $sql = "INSERT INTO usuarios (nombre, estado) VALUES ('$nombre', '$estado')";
+
+    if (mysqli_query($mysqli, $sql)) {
+        echo "Se ha registrado correctamente el usuario";
+    }
+    else {
+        echo "Error al registrar: " . mysqli_error($mysqli);
+    }
+
+    $id = 115;
+    $estado1 = "1";
+
+    $sql = "UPDATE usuarios SET estado = '$estado1' WHERE id = '$id'";
+
+
+    if ($mysqli->query($sql) === TRUE) {
+        echo "Usuario actualizado con exito";
+    }
+    else {
+        echo "Error al actualizar: " . $mysqli->error;
+    }
+
+    $id = 5;
+
+    $sql = "DELETE FROM usuarios WHERE id = $id";
+
+    if ($mysqli->query($sql) === TRUE) {
+        echo "Usuario eliminado con exito";
+    }
+    else {
+        echo "Error al eliminar usuario: " . $mysqli->error;
+    }
+
+    
